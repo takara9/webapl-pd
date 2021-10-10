3400cdf3850c8bc609ed3a47434fda3c47ef0739
Maho Takara <takara@labo.local>
2021-10-10 09:42:09 +0900

    update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req._remoteAddress);
+    var networkInterfaces = os.networkInterfaces();        
     res.render('index', {
 	hostname: process.env.HOSTNAME,
-	ip: req._remoteAddress,
+	ip: networkInterfaces.eth0[0].cidr,	
 	host: req.headers.host,
 	agent: req.headers['user-agent'],
     });
--- myapp/routes/info.js
+++ myapp/routes/info.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
+
 
 router.get('/', function(req, res, next) {
+    var networkInterfaces = os.networkInterfaces();    
+    //console.log(networkInterfaces.eth0[0].cidr);
     msg = {
 	hostname: process.env.HOSTNAME,
-	ip: req._remoteAddress,
+	ip: networkInterfaces.eth0[0].cidr,
 	host: req.headers.host,
 	agent: req.headers['user-agent'],
     }	
